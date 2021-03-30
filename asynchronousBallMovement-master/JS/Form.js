4fd8c25fb65fb7c934dd19a099cbe311b766fd73
Rushil9128 <73953157+Rushil9128@users.noreply.github.com>
2021-03-30 20:49:27 +0530

    Add files via upload

---
+++ asynchronousBallMovement-master/JS/Form.js
@@ -0,0 +1,37 @@
+class Form{
+    constructor(){
+        this.input = createInput('name');
+        this.button = createButton('PLAY');
+        this.greeting = createElement('h2');
+        this.title = createElement('h1');
+    }
+    hide(){
+        this.greeting.hide();
+        this.button.hide();
+        this.input.hide();
+        
+    }
+    display(){
+        
+       this.title.html('car racing game');
+       this.title.position(displayWidth/2-50,0);
+       
+       this.input.position(displayWidth/2-40,displayHeight/2-80);
+       
+       this.button.position(displayWidth/2+30,displayHeight/2);
+      
+       this.button.mousePressed(()=>{
+           this.input.hide();
+           this.button.hide();
+           this.button.position(1000,1000);
+           player.name = this.input.value();
+           playerCount++;
+           player.index = playerCount;
+           player.update();
+           player.updateCount(playerCount);
+           this.greeting.html("hello!! " + player.name);
+           this.greeting.position(displayWidth/2-70,displayHeight/4);
+        
+       });
+    }
+}
